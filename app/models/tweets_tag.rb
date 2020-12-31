b702a0224f9f65924a8b6328b3e4975cb029a31b
erika618 <68714247+erika618@users.noreply.github.com>
2020-12-31 11:59:51 +0900

    タグについて保存方法を変更（Fromモデル）
    従来の記述では、かこにツイートした内容についてタグ付ができないため。

--- app/models/tweets_tag.rb
+++ app/models/tweets_tag.rb
@@ -9,7 +9,11 @@ class TweetsTag
 
   def save
     tweet = Tweet.create(message: message)
-    tag = Tag.create(name: name)
+    # tag = Tag.create(name: name)→これでは過去のデータのタグ付ができない欠点あり
+    # 既存のタグが存在する場合には既存のタグを反映し、なければインスタンスを生成または保存するメソッドを使用する。
+    # tag = Tag.where(name: name).first_or_initialize
+    # tag.save
+    tag = Tag.where(name: name).first_or_create
 
     TweetTagRelation.create(tweet_id: tweet.id, tag_id: tag.id)
   end
