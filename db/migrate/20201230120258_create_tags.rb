33ab8e602d2750dcad2e28e27551cde28bfec7d2
erika618 <68714247+erika618@users.noreply.github.com>
2020-12-31 11:08:11 +0900

    マイグレーションファイルの編集。
    編集後、ルボコップを実行。

--- db/migrate/20201230120258_create_tags.rb
+++ db/migrate/20201230120258_create_tags.rb
@@ -1,7 +1,8 @@
 class CreateTags < ActiveRecord::Migration[6.0]
   def change
     create_table :tags do |t|
-
+      # タグの重複を防ぐため
+      t.string :name, null:false, uniqueness: true
       t.timestamps
     end
   end
